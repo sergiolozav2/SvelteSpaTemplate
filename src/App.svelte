374c212894be2c856f4c445caa354ae1c2107eb1
Sergio Loza <sergiolozav@gmail.com>
2024-04-17 22:18:28 -0400

    feat: SPA Svelte Template created

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import "./app.css";
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <div class="min-h-screen grid place-items-center">
+    <Counter />
+  </div>
+</main>
+
+<style>
+</style>
